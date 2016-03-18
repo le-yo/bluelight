c6a7b5ff482406f729049155fb3e05711feff0b5
Le-Yo <lenykoskey@yahoo.com>
2016-03-18 23:02:49 +0300

    initial commit

--- include/config.php
+++ include/config.php
@@ -5,7 +5,7 @@
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'gcm_chat_new');
+define('DB_NAME', 'gcm_chat');
 
 define("GOOGLE_API_KEY", "AIzaSyDKk_ew7Vi4FZnMzu6GCY5nLb4xZG8muvI");
 
